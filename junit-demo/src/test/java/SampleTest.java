fbc8e14666dad9b66d84a8eff58e3c0fba61d662
Admin <Admin@10.0.0.49>
2022-03-27 09:49:06 -0400

    first commit

---
+++ junit-demo/src/test/java/SampleTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SampleTest {
+	@Test
+    public void simpleTestPass() {
+        int a = 1;
+        int b = 2;
+        assertTrue(a + b == 3);
+    }
+    @Test
+    public void simpleTestFail() {
+        int a = 1;
+        int b = 2;
+        assertTrue(a + b == 3);
+    }
+}
